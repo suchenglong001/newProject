87e79f3d465fc558177e024a0f7a264c2d2f34e2
rb yu <rbyu@rbdeMacBook-Air.local>
2018-02-26 13:40:12 +0800

    ok

--- src/android/components/demageInfo/demageOpResult/DemageOpResult.js
+++ src/android/components/demageInfo/demageOpResult/DemageOpResult.js
@@ -16,24 +16,64 @@ const DemageOpResult = props => {
         op_user_name, company_cost, under_cost, profit, penalty_cost, reduction_cost, transport_cost, repair_cost, refund_user_name, under_user_name, damage_type, damage_link_type } } },
         damageStatus } = props
     return (
-        <Content showsVerticalScrollIndicator={false}>
+        <Content showsVerticalScrollIndicator={false} style={{ backgroundColor: '#eaeef1' }}>
             {damageStatus == 3 && <View style={styles.body}>
-                <Text style={[styles.text, globalStyles.midText]}><Text style={styles.title}>质损类型：</Text>{damage_type ? `${damageTypeList.find(item => item.id == damage_type).value}` : ''}</Text>
-                <Text style={[styles.text, globalStyles.midText]}><Text style={styles.title}>责任人：</Text>{under_user_name ? `${under_user_name}` : ''}</Text>
-                <Text style={[styles.text, globalStyles.midText]}><Text style={styles.title}>报销人：</Text>{refund_user_name ? `${refund_user_name}` : ''}</Text>
-                <Text style={[styles.text, globalStyles.midText]}><Text style={styles.title}>质损环节：</Text>{damage_link_type ? `${damageLinkTypeList.find(item => item.id == damage_link_type).value}` : ''}</Text>
-                <Text style={[styles.text, globalStyles.midText]}><Text style={styles.title}>维修费：¥</Text> {repair_cost ? `${moneyFormat(repair_cost, 2)}` : '0.00'}元</Text>
-                <Text style={[styles.text, globalStyles.midText]}><Text style={styles.title}>运费：¥</Text> {transport_cost ? `${moneyFormat(transport_cost, 2)}` : '0.00'}元</Text>
-                <Text style={[styles.text, globalStyles.midText]}><Text style={styles.title}>降价费：¥</Text> {reduction_cost ? `${moneyFormat(reduction_cost, 2)}` : '0.00'}元</Text>
-                <Text style={[styles.text, globalStyles.midText]}><Text style={styles.title}>罚款：¥</Text> {penalty_cost ? `${moneyFormat(penalty_cost, 2)}` : '0.00'}元</Text>
-                <Text style={[styles.text, globalStyles.midText]}><Text style={styles.title}>盈亏：¥</Text> {profit ? `${moneyFormat(profit, 2)}` : '0.00'}元</Text>
-                <View style={[styles.text, styles.strikingItem]}>
-                    <Text style={[styles.title, globalStyles.midText]}>责任人承担费用：</Text>
-                    <Text style={globalStyles.largeText}>¥ <Text style={styles.strikingText}>{under_cost ? `${moneyFormat(under_cost, 2)}` : '0.00'}</Text> 元</Text>
+                <View style={styles.group}>
+                    <View style={styles.strikingItem}>
+                        <Text style={[styles.text, globalStyles.midText, styles.title]}>基本信息</Text>
+                    </View>
+                    <View style={styles.strikingItem}>
+                        <Text style={[styles.text, globalStyles.midText, styles.title]}>质损类型：</Text>
+                        <Text style={[styles.text, globalStyles.midText]}>{damage_type ? `${damageTypeList.find(item => item.id == damage_type).value}` : ''}</Text>
+                    </View>
+                    <View style={styles.strikingItem}>
+                        <Text style={[styles.text, globalStyles.midText, styles.title]}>责任人：</Text>
+                        <Text style={[styles.text, globalStyles.midText]}>{under_user_name ? `${under_user_name}` : ''}</Text>
+                    </View>
+                    <View style={styles.strikingItem}>
+                        <Text style={[styles.text, globalStyles.midText, styles.title]}>报销人：</Text>
+                        <Text style={[styles.text, globalStyles.midText]}>{refund_user_name ? `${refund_user_name}` : ''}</Text>
+                    </View>
+                    <View style={styles.strikingItem}>
+                        <Text style={[styles.text, globalStyles.midText, styles.title]}>质损环节：</Text>
+                        <Text style={[styles.text, globalStyles.midText]}>{damage_link_type ? `${damageLinkTypeList.find(item => item.id == damage_link_type).value}` : ''}</Text>
+                    </View>
                 </View>
-                <View style={[styles.text, styles.strikingItem]}>
-                    <Text style={[styles.title, globalStyles.midText]}>公司承担费用：</Text>
-                    <Text style={globalStyles.largeText}>¥ <Text style={styles.strikingText}>{company_cost ? `${moneyFormat(company_cost, 2)}` : '0.00'}</Text> 元</Text>
+                <View style={styles.group}>
+                    <View style={styles.strikingItem}>
+                        <Text style={[styles.text, globalStyles.midText, styles.title]}>费用信息</Text>
+                    </View>
+                    <View style={styles.strikingItem}>
+                        <Text style={[styles.text, globalStyles.midText, styles.title]}>维修费：</Text>
+                        <Text style={[styles.text, globalStyles.midText]}>{repair_cost ? `${moneyFormat(repair_cost, 2)}` : '0.00'}元</Text>
+                    </View>
+                    <View style={styles.strikingItem}>
+                        <Text style={[styles.text, globalStyles.midText, styles.title]}>运费：</Text>
+                        <Text style={[styles.text, globalStyles.midText]}>{transport_cost ? `${moneyFormat(transport_cost, 2)}` : '0.00'}元</Text>
+                    </View>
+                    <View style={styles.strikingItem}>
+                        <Text style={[styles.text, globalStyles.midText, styles.title]}>降价费：</Text>
+                        <Text style={[styles.text, globalStyles.midText]}>{reduction_cost ? `${moneyFormat(reduction_cost, 2)}` : '0.00'}元</Text>
+                    </View>
+                    <View style={styles.strikingItem}>
+                        <Text style={[styles.text, globalStyles.midText, styles.title]}>罚款：</Text>
+                        <Text style={[styles.text, globalStyles.midText]}>{penalty_cost ? `${moneyFormat(penalty_cost, 2)}` : '0.00'}元</Text>
+                    </View>
+                    <View style={styles.strikingItem}>
+                        <Text style={[styles.text, globalStyles.midText, styles.title]}>盈亏：</Text>
+                        <Text style={[styles.text, globalStyles.midText]}>{profit ? `${moneyFormat(profit, 2)}` : '0.00'}元</Text>
+                    </View>
+                </View>
+                <View style={styles.group}>
+                    <Text style={[styles.text, globalStyles.midText, styles.title]}>责任信息</Text>
+                    <View style={[styles.text, styles.strikingItem]}>
+                        <Text style={[styles.title, globalStyles.midText]}>责任人承担费用：</Text>
+                        <Text style={globalStyles.largeText}><Text style={styles.strikingText}>{under_cost ? `${moneyFormat(under_cost, 2)}` : '0.00'}</Text> 元</Text>
+                    </View>
+                    <View style={[styles.text, styles.strikingItem]}>
+                        <Text style={[styles.title, globalStyles.midText]}>公司承担费用：</Text>
+                        <Text style={globalStyles.largeText}><Text style={styles.strikingText}>{company_cost ? `${moneyFormat(company_cost, 2)}` : '0.00'}</Text> 元</Text>
+                    </View>
                 </View>
             </View>}
             <View style={styles.footer}>
@@ -70,16 +110,19 @@ const styles = StyleSheet.create({
         color: 'red'
     },
     body: {
-        paddingVertical: 15,
-        marginHorizontal: 15
+        margin: 7
     },
     footer: {
-        borderTopWidth: 0.3,
-        borderColor: '#777',
         paddingVertical: 15,
-        marginTop:15,
         marginHorizontal: 15,
         alignItems: 'flex-end'
+    },
+    group: {
+        backgroundColor: '#fff',
+        padding: 10,
+        margin: 7,
+        borderColor: '#eee',
+        borderWidth: 0.3
     }
 })
 
